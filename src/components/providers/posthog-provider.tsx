e4159696867706388c0715dfd817e92d5e477289
codeREDxbt <codeREDxbt@users.noreply.github.com>
2026-07-09 16:16:21 +0530

    fix: remove local proxy for posthog

--- src/components/providers/posthog-provider.tsx
+++ src/components/providers/posthog-provider.tsx
@@ -28,7 +28,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     if (process.env.NEXT_PUBLIC_POSTHOG_KEY && process.env.NEXT_PUBLIC_POSTHOG_HOST) {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-        api_host: "/ingest",
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
         ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
         person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
         capture_pageview: false, // Disable automatic pageview capture, as we capture manually
